64dcba0e692618c8e01a0e511087acbf59b1d090
poornima sewak <poornima.mathur.20@gmail.com>
2019-01-24 11:17:32 -0600

    added new chat app

---
+++ react-intro/src/config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCMpVKutfCIoVm_3JfMaP6yuW8bAiLnJBI",
+    authDomain: "chat-app-bf257.firebaseapp.com",
+    databaseURL: "https://chat-app-bf257.firebaseio.com",
+    projectId: "chat-app-bf257",
+    storageBucket: "chat-app-bf257.appspot.com",
+    messagingSenderId: "763960684526"
+};
