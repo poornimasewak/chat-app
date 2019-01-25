6886598af59af346bb4047a339cd9004f1dca916
poornima sewak <poornima.mathur.20@gmail.com>
2019-01-25 14:19:12 -0600

    added domain

--- react-intro/src/config.js
+++ react-intro/src/config.js
@@ -1,6 +1,6 @@
 export default {
     apiKey: "AIzaSyCMpVKutfCIoVm_3JfMaP6yuW8bAiLnJBI",
-    authDomain: "chat-app-bf257.firebaseapp.com",
+    authDomain: "react-firebase-chat-app.herokuapp.com",
     databaseURL: "https://chat-app-bf257.firebaseio.com",
     projectId: "chat-app-bf257",
     storageBucket: "chat-app-bf257.appspot.com",
